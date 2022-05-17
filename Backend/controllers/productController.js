1449096bb097a3ab3d6e5567f14062c4d8745ed5
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-05-17 21:40:51 +0200

    finished the products api making it fully restful

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -29,4 +29,101 @@ exports.getAll = async (req, res) =>{
     }
 }
 
+exports.createProduct = async (req, res) =>{
+    try{
+        const newProduct = await products.create(req.body)
+        
+        res.status(200).json({
+            status: 'success',
+            data:{
+                newProduct
+            }
+        })
+    
+    }catch(err){
+    
+        res.status(404).json({
+            status: 'failed',
+            message : err.message
+        })
+    }
 
+
+}
+
+exports.deleteProductAll = async (req, res) =>{
+    try{
+    await products.deleteMany();
+    
+    res.status(200).json({
+        status: 'success',
+        data:"data deleted successfully"
+    })
+    }
+    catch(err){
+        res.status(404).json({
+            status: 'failed',
+            message : err.message
+        })
+    }
+}
+
+exports.getProduct = async (req, res) =>{
+    try{
+        const product = await products.findById(req.params.id);
+        
+        res.status(200).json({
+            status: 'success',
+            data:product
+        })
+    
+    }catch(err){
+        res.status(404).json({
+            status: 'failed',
+            message : err.message
+        })
+    }
+
+}
+
+exports.deleteProduct = async (req, res) =>{
+    try{
+        await products.findByIdAndDelete(req.params.id)
+        
+        res.status(200).json({
+            status: 'success',
+            data:"data deleted successfully"
+        })
+    
+    }catch(err){
+        res.status(404).json({
+            status: 'failed',
+            message : err.message
+        })
+    }
+
+}
+
+
+exports.updateProduct = async (req, res) =>{
+    try{
+        const newData = await products.findByIdAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true
+        })
+    
+        res.status(200).json({
+            status: 'success',
+            data:newData
+        })
+    
+    }catch(err){
+        res.status(404).json({
+            status: 'failed',
+            message : err.message
+        })
+    
+    }
+
+
+}
--- Backend/routes/productsRoutes.js
+++ Backend/routes/productsRoutes.js
@@ -7,6 +7,16 @@ const router = express.Router();
 router
     .route('/')
     .get(productController.getAll)
+    .post(productController.createProduct)
+    .delete(productController.deleteProductAll)
+    
+router
+    .route('/:id')
+    .get(productController.getProduct)
+    .delete(productController.deleteProduct)
+    .patch(productController.updateProduct)
+
+
 
 
 
