5376bbaa9f28a5ebc160e5bf86182592293d5c23
AlHassanHK <al.hassan.elkady@gmail.com>
2022-05-19 12:39:34 +0200

    added order controller

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -24,3 +24,53 @@ exports.getAll = async (req, res) => {
     });
   }
 };
+
+exports.createOrder = async (req, res) => {
+  try {
+    const newOrder = await orders.create(req.body);
+    res.status(201).json({
+      status: "Successfully created order.",
+      data: { newOrder },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Bad request.",
+      data: {},
+    });
+  }
+};
+
+exports.updateOrder = async (req, res) => {
+  try {
+    const updateOrder = await orders.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    res.status(200).json({
+      status: "Successfully updated product.",
+      data: { updateOrder },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Bad request",
+      data: {},
+    });
+  }
+};
+
+exports.cancelOrder = async (req, res) => {
+  try {
+    const cancelledOrder = await orders.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    res.status(200).json({
+      status:"Successfully cancelled order",
+      data:{
+        cancelledOrder
+      }
+    });
+  } catch (error) {}
+};
--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const orderController = require('../controllers/orderController')
+const express = require("express");
+const orderController = require("../controllers/orderController");
 
 const router = express.Router();
 
-
 router
-    .route('/')
-    .get(orderController.getAll)
+  .route("/")
+  .get(orderController.getAll)
+  .post(orderController.createOrder);
 
+router.route("/:id").patch(orderController.updateOrder);
 
-
-
-
+router.route("cancel/:id", orderController.cancelOrder);
 
 module.exports = router;
