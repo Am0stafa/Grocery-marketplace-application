349f023377d67768e1ed68b8b4801de0c04d0981
AlHassanHK <al.hassan.elkady@gmail.com>
2022-05-21 11:03:48 +0200

    database and server now start simultaneously

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,21 @@
 //! 1) connect to database and run app
 
-const dotenv = require('dotenv')
-dotenv.config({path: "./config.env"})
-const app = require('./app')
-const mongoose = require('mongoose')
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+const mongoose = require("mongoose");
 
-const DB = process.env.DATABASE.replace('<password>',process.env.PASSWORD)
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-  }).then(con =>{
-    console.log('DB is connected')
-  }).catch(err => console.log(err))
-  
-const port = process.env.PORT || 3000
-const server = app.listen(port,()=>{
-    console.log("APP is running")
-})
+const DB = process.env.DATABASE.replace("<password>", process.env.PASSWORD);
+const port = process.env.PORT || 3000;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("DB is connected");
+    const server = app.listen(port, () => {
+      console.log("APP is running");
+    });
+  })
+  .catch((err) => console.log(err));
